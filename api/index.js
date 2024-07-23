c6193a6ed0d3d25b5152bad9c9a273c8cb8540e5
react-redux-project <wm635950@gmail.com>
2024-07-23 18:10:48 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
 const app = express();
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongoDB")
+}).catch((err)=>{
+    console.log(err);
+})
 app.listen(3000,()=>{
 console.log("server in runing on port 3000")
 });
+
+
+
+// mongodb+srv://waqas:waqas2021-ag-5936skp@mern-estate.b2ukjdf.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate
