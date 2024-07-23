3c68c2162f09a17ff10559619a0e38c7e768e799
react-redux-project <wm635950@gmail.com>
2024-07-23 23:17:40 +0500

    create sign in api

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,18 +1,44 @@
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 
 
+export const signup = async (req, res, next) => {
 
-export const signup=async(req,res,next)=>{
+    const { username, email, password } = req.body;
+    const hashedPasswrod = bcrypt.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPasswrod });
 
-const{username,email,password}=req.body;
-const hashedPasswrod =bcrypt.hashSync(password,10);
-const newUser=new User({username,email,password:hashedPasswrod});
-
-try {
-    await newUser.save()
-res.status(201).json("user created successfully")
-} catch (error) {
-   next(error);
+    try {
+        await newUser.save()
+        res.status(201).json("user created successfully")
+    } catch (error) {
+        next(error);
+    }
 }
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "user not found"));
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+
+        if (!validPassword) return next(errorHandler(401, 'wrong credentials!'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        const { password: pass, ...rest } = validUser.toObject();
+
+
+        res.cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+
+
+
+    } catch (error) {
+        next(error)
+    }
 }
