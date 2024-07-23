a42e3618aa230289e486652a27587bd558376ad9
react-redux-project <wm635950@gmail.com>
2024-07-23 10:25:14 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter ,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
 
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
 const App = () => {
   
-  return (
-    <div className='text-3xl font-bold underline'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route  path='/' element={<Home/>} />
+    
+    <Route  path='/sign-out' element={<SignOut/>} />
+    <Route  path='/profile' element={<Profile/>} />
+    <Route  path='/about' element={<About/>} />
+  </Routes>
+  </BrowserRouter>
+  
 }
 
 export default App
