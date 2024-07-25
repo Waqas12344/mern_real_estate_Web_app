dae6d53993382774ed51d49b6100710cfbb2d6e6
react-redux-project <wm635950@gmail.com>
2024-07-25 12:04:43 +0500

    add redux persist

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -35,9 +35,6 @@ export const signin = async (req, res, next) => {
         res.cookie('access_token', token, { httpOnly: true })
             .status(200)
             .json(rest);
-
-
-
     } catch (error) {
         next(error)
     }
