1a02a9e3e3ece7e7a5443051b852127ca0e72035
react-redux-project <wm635950@gmail.com>
2024-07-27 12:59:59 +0500

    add private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 const App = () => {
   
   return <BrowserRouter>
@@ -15,8 +16,12 @@ const App = () => {
     <Route  path='/' element={<Home/>} />
  <Route path='/sign-in' element={<SignIn/>} />
     <Route  path='/sign-up' element={<SignUp/>} />
-    <Route  path='/profile' element={<Profile/>} />
     <Route  path='/about' element={<About/>} />
+    <Route element={<PrivateRoute/>} >
+    <Route  path='/profile' element={<Profile/>} />
+
+    </Route>
+
   </Routes>
   </BrowserRouter>
   
