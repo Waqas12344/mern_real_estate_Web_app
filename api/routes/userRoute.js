e0d23c19f9c2750682c0f5f686b430cd55ff3e5b
react-redux-project <wm635950@gmail.com>
2024-08-11 12:30:09 +0500

    add delete user funcationality

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { test ,updateUser} from '../controller/userController.js';
+import { test ,updateUser,deleteUser} from '../controller/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router =express.Router();
 
 router.get('/signup',test)
 router.post('/update/:id',verifyToken,updateUser)
+router.delete('/delete/:id',verifyToken,deleteUser)
 
 export default router;
