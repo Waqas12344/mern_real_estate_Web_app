2216e08f575d833c631af9c784197104fcfeb2f5
react-redux-project <wm635950@gmail.com>
2024-08-14 21:46:57 +0500

    create update api

--- api/controller/listingController.js
+++ api/controller/listingController.js
@@ -1,4 +1,7 @@
+import mongoose from "mongoose";
+
 import Listing from "../models/listingModel.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
 
@@ -9,6 +12,7 @@ export const createListing = async (req, res, next) => {
         next(error)
     }
 }
+
 
 export const deleteListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
@@ -26,3 +30,35 @@ export const deleteListing = async (req, res, next) => {
         next(error)
     }
 }
+
+
+
+
+export const updateListing = async (req, res, next) => {
+    // Check if the ID is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return next(errorHandler(400, 'Invalid listing ID'));
+    }
+
+    try {
+        const listing = await Listing.findById(req.params.id);
+
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found'));
+        }
+
+        if (req.user.id !== listing.userRef) {
+            return next(errorHandler(401, 'You can only update your own listings!'));
+        }
+
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true }
+        );
+
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+};
